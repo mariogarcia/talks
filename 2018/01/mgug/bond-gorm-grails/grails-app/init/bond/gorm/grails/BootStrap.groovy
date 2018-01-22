a16bfce37408f9e15f8299c0788ca9e8f74dd59d
Mario Garcia <mario.garcia@kaleidos.net>
2018-01-23 00:06:00 +0100

    Add Bond movies to Grails in memory database

--- 2018/01/mgug/bond-gorm-grails/grails-app/init/bond/gorm/grails/BootStrap.groovy
+++ 2018/01/mgug/bond-gorm-grails/grails-app/init/bond/gorm/grails/BootStrap.groovy
@@ -1,9 +1,17 @@
 package bond.gorm.grails
 
+import bond.Film
+import bond.db.Queries
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+    Queries
+      .loadDataset()
+      .collect { new Film(it) }
+      .each { it.save() }
+  }
+
+  def destroy = {
+  }
 }
