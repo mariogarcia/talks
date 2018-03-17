da50e25739b928d6baec7706c690fc82f76f6a2a
Mario Garcia <mario.garcia@kaleidos.net>
2018-03-17 03:52:24 +0100

    Add comments to basic

--- 2018/03/greach/slides/src/cheatsheet/gql-basic.groovy
+++ 2018/03/greach/slides/src/cheatsheet/gql-basic.groovy
@@ -3,10 +3,14 @@ import gql.DSL
 
 RAFFLES = evaluate('gql-data-raffles.groovy' as File)
 
+// 1. DEFINE data types
 def Contestant = null
 def Raffle = null
+
+// 2 & 3. DEFINE queries & schema
 def Schema = null
 
+// 4. EXECUTE query
 def query = '''
  {
 
